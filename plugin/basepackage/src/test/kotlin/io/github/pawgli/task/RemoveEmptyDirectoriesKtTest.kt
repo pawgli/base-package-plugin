0a25ba1de77e88a89e52f88a04a942d20824a562
pawgli <pawel.gliniecki@droidsonroids.pl>
2024-07-25 15:13:46 +0200

    test removing empty directories

---
+++ plugin/basepackage/src/test/kotlin/io/github/pawgli/task/RemoveEmptyDirectoriesKtTest.kt
@@ -0,0 +1,56 @@
+package io.github.pawgli.task
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+class RemoveEmptyDirectoriesKtTest {
+
+  @TempDir
+  lateinit var testFolder: File
+
+  @BeforeEach
+  fun setUp() {
+    TestData.initializeTestDirTree(testFolder)
+  }
+
+  @Test
+  fun `should remove empty directories`() {
+    removeEmptyDirectories(testFolder)
+
+    val actualDirTree = testFolder.walkTopDown()
+      .map { it.relativeTo(testFolder).toString() }
+      .filter { it.isNotEmpty() } // Root directory
+      .toList()
+
+    actualDirTree shouldBe TestData.expectedDirTree
+  }
+}
+
+private object TestData {
+
+  fun initializeTestDirTree(folder: File) {
+    File(folder, "rootLevelEmptyDir").mkdir()
+
+    val rootLevelNonEmptyDir = File(folder, "rootLevelNonEmptyDir")
+    rootLevelNonEmptyDir.mkdir()
+    File(rootLevelNonEmptyDir, "File.kt").createNewFile()
+
+    File(folder, "nested").mkdir()
+    File(folder, "nested/emptyDir").mkdir()
+
+    val nestedNonEmptyDir = File(folder, "nested/nonEmptyDir")
+    nestedNonEmptyDir.mkdir()
+    File(nestedNonEmptyDir, "file.txt").createNewFile()
+  }
+
+  val expectedDirTree = listOf(
+    "rootLevelNonEmptyDir",
+    "rootLevelNonEmptyDir/File.kt",
+    "nested",
+    "nested/nonEmptyDir",
+    "nested/nonEmptyDir/file.txt",
+  )
+}
