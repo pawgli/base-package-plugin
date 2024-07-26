d57160c85fea20ea951e5266e00a7e495cacf7dc
pawgli <pawel.gliniecki@droidsonroids.pl>
2024-07-26 10:48:25 +0200

    add unit tests for a withRetry util

--- plugin/basepackage/src/test/kotlin/io/github/pawgli/utils/WithRetryKtTest.kt
+++ plugin/basepackage/src/test/kotlin/io/github/pawgli/utils/WithRetryKtTest.kt
@@ -1,5 +1,50 @@
-package io.github.pawgli.utils
+import io.github.pawgli.utils.withRetry
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
+class WithRetryKtTest {
 
-class WithRetryKtTest
+  @Test
+  fun `withRetry returns true when action succeeds on first try`() {
+    var actionCount = 0
+    val action = { actionCount++; true }
+
+    val result = withRetry(tries = 3, action)
+
+    result shouldBe true
+    actionCount shouldBe 1
+  }
+
+  @Test
+  fun `withRetry returns true when action fails first but succeeds on second try`() {
+    var actionCount = 0
+    val action = { actionCount++; actionCount == 2 }
+
+    val result = withRetry(tries = 3, action)
+
+    result shouldBe true
+    actionCount shouldBe 2
+  }
+
+  @Test
+  fun `withRetry returns true when action fails first but succeeds on fourth try`() {
+    var actionCount = 0
+    val action = { actionCount++; actionCount == 4 }
+
+    val result = withRetry(tries = 4, action)
+
+    result shouldBe true
+    actionCount shouldBe 4
+  }
+
+  @Test
+  fun `withRetry returns false when action fails all tries`() {
+    var actionCount = 0
+    val action = { actionCount++; false }
+
+    val result = withRetry(tries = 3, action)
+
+    result shouldBe false
+    actionCount shouldBe 3
+  }
+}
